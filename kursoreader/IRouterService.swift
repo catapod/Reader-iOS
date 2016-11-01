52f772c34a4dcd60d0a900afbef368c7f53e8ab3
Igorfn <ifeshun@gmail.com>
2016-11-01 16:58:38 +0300

    Added router service

---
+++ kursoreader/IRouterService.swift
@@ -0,0 +1,16 @@
+//
+//  IRouterService.swift
+//  kursoreader
+//
+//  Created by Igor Feshchun on 19.10.16.
+//  Copyright © 2016 Kursomir. All rights reserved.
+//
+
+import Foundation
+
+protocol IRouterService: class {
+    
+    func showWelcome()
+    func showMain()
+    
+}
