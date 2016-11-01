d82fab9532441c447d6d8ca4a1820ed285588efe
Igorfn <ifeshun@gmail.com>
2016-11-01 17:00:31 +0300

    Added design file

---
+++ kursoreader/Design.swift
@@ -0,0 +1,39 @@
+//
+//  Design.swift
+//  kursoreader
+//
+//  Created by Nikita Feshchun on 29.09.16.
+//  Copyright © 2016 Kursomir. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+extension UIColor {
+    class func color1() -> UIColor{
+        return UIColor(red: 238.0 / 255, green: 65.0 / 255, blue: 54.0 / 255, alpha: 1.0)
+    }
+    
+    class func color2() -> UIColor{
+        return UIColor(red: 241.0 / 255, green: 103.0 / 255, blue: 94.0 / 255, alpha: 1.0)
+    }
+    
+    class func color3() -> UIColor{
+        return UIColor(red: 200.0 / 255, green: 199.0 / 255, blue: 204.0 / 255, alpha: 1.0)
+    } // grey separator
+    
+    class func color4() -> UIColor{
+        return UIColor(red: 247.0 / 255, green: 247.0 / 255, blue: 247.0 / 255, alpha: 1.0)
+    } // grey background
+    
+    class func color5() -> UIColor{
+        return UIColor(red: 132.0 / 255, green: 132.0 / 255, blue: 132.0 / 255, alpha: 1.0)
+    } // grey label
+    
+}
+
+extension UIImageView {
+    func configureImageView(with image: UIImage){
+        
+    }
+}
